ab4cf48e58d5e3c3669abaf34208d12da78b0564
Ashley James <ashley@dashmage.dev>
2024-05-26 21:15:12 +0530

    First lousy but working attempt

--- plr/runner.py
+++ plr/runner.py
@@ -20,7 +20,12 @@ class Validator:
     @property
     def solution_methods(self) -> list:
         attrs = list(vars(self.solution.__class__))
-        return [a for a in attrs if not a.startswith("_")]
+        return [a for a in attrs if not a.startswith("_") and a != "test"]
+
+    @property
+    def custom_test_exists(self):
+        attrs = list(vars(self.solution.__class__))
+        return True if "test" in attrs else False
 
     def parse_examples(self):
         examples = []
@@ -43,7 +48,27 @@ class Validator:
             return False
         if output.lower() == "true":
             return True
-        return eval(output)
+        result = ()
+        if len(output_elements := output.split(", ")) > 1:
+            for element in output_elements:
+                if "=" in element:
+                    value = element.split("=")[1].replace(",_", "")
+                    result = result + (eval(value),)
+                else:
+                    result = result + (eval(element),)
+        else:
+            result = eval(output)
+        return result
+
+    def parse_string_to_kwargs(self, s):
+        # assuming each pair of kwargs are separated by ", "
+        # and any list, set, dict doesn't have space after the comma
+        pairs = s.split(", ")
+        kwargs = {}
+        for pair in pairs:
+            key, value = pair.split("=")
+            kwargs[key.strip()] = eval(value)
+        return kwargs
 
     def check(self, extra_cases: Optional[List[TestCase]] = None) -> List[bool]:
         extra_cases = extra_cases or []
@@ -53,7 +78,16 @@ class Validator:
             print_heading(f"\n=== {method_name} ===\n")
             for input_str, output_str in self.test_cases:
                 expected = self.eval_output(output_str)
-                actual = eval(f"self.solution.{method_name}({input_str})")
+
+                method = getattr(self.solution, method_name)
+                input_kwargs = self.parse_string_to_kwargs(input_str)
+                if self.custom_test_exists:
+                    test_method = getattr(self.solution, "test")
+                    actual = test_method(method, input_kwargs)
+                else:
+                    actual = method(**input_kwargs)
+
+                # actual = eval(f"self.solution.{method_name}({input_str})")
 
                 print_case_summary(input_str, actual, expected)
                 results.append(actual == expected)
